b1bd2332d6912262cd185b7b0f2764ef49683a6c
Dave Caulton <dcaulton@gmail.com>
2026-01-09 20:50:15 -0600

    adds channels and programs ok now

--- src/app.py
+++ src/app.py
@@ -43,16 +43,17 @@ CREATE TABLE IF NOT EXISTS channel (
 CREATE TABLE IF NOT EXISTS schedule (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     channel_id INTEGER NOT NULL,
+    program_id INTEGER NOT NULL,
     start_date TEXT NOT NULL,          -- ISO 8601: '2026-01-12T06:00:00+0000'
     end_date TEXT NOT NULL,            -- ISO 8601
     FOREIGN KEY (channel_id) REFERENCES channel(id) ON DELETE CASCADE,
+    FOREIGN KEY (program_id) REFERENCES program(id) ON DELETE CASCADE,
     UNIQUE(channel_id, start_date)
 );
 """
     create_program = """
 CREATE TABLE IF NOT EXISTS program (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    schedule_id INTEGER NOT NULL,
     sd_programid TEXT NOT NULL,        -- e.g. "EP017254570124"
     title TEXT NOT NULL,
     description TEXT,
@@ -60,8 +61,7 @@ CREATE TABLE IF NOT EXISTS program (
     original_air_date TEXT,            -- ISO 8601 date only: '2025-01-10'
     season INTEGER,                    -- nullable
     episode INTEGER,                   -- nullable
-    FOREIGN KEY (schedule_id) REFERENCES schedule(id) ON DELETE CASCADE,
-    UNIQUE(schedule_id, sd_programid)
+    UNIQUE(sd_programid)
 );
 """
     create_person = """
@@ -85,37 +85,151 @@ CREATE TABLE IF NOT EXISTS person (
     print('db init complete')
 
 def add_channels(channels):
+    add_counter = 0
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.execute("select station_id from channel;")
     rows = cursor.fetchall()
     cur_chan_station_ids = [x[0] for x in rows]
-    print(f"current channels are {cur_chan_station_ids}")
     cursor = conn.cursor()
     for channel in channels:
       if channel.get('stationID') not in cur_chan_station_ids:
         sid = channel.get('stationID')
         channel_dot_number = channel.get('channel')
         cursor.execute(f"INSERT INTO channel (station_id, channel) VALUES (?, ?);", (sid, channel_dot_number))
-        print(f'adding channel {channel_dot_number}')
-#    conn.execute("INSERT INTO channel (station_id, channel) VALUES ('I30415.json.schedulesdirect.org', '2.1');")
+        add_counter += 1
+#        print(f'adding channel {channel_dot_number}')
     conn.commit(); 
     conn.close()
-    print('channels add complete')
+    print(f'{len(channels)} channels processed, {add_counter} added')
+
+def get_channels(conn):
+    cursor = conn.execute("select id, station_id from channel;")
+    rows = cursor.fetchall()
+    channels = {}
+    for x in rows:
+      channels[x[0]] = x[1]  # key = sql id of the channel,    value = station id of the channel in schedule direct
+#    print(f"current channels are {channels}")
+    return channels
+
+def get_programs(conn):
+    cursor = conn.execute("select id, sd_programid from program;")
+    rows = cursor.fetchall()
+    programs = {}
+    for x in rows:
+      programs[x[0]] = x[1]  # key = sql id of the program,    value = program id of the channel in schedule direct
+    print(f"current programs are {programs}")
+    return programs
+
+def get_schedule_dates(conn):
+    cursor = conn.execute("select id, start_date from schedule;")
+    rows = cursor.fetchall()
+    cur_schedules = {}
+    for x in rows:
+      sched_id = x[0]
+      start_date = x[1]
+      if sched_id not in cur_schedules:
+        cur_schedules[sched_id] = []
+      if start_date not in cur_schedules[sched_id]:
+        cur_schedules[sched_id].append(start_date)
+    print(f"current schedules are {cur_schedules}")
+    return cur_schedules
+
+def get_schedule_dates_by_chan_id(conn):
+    cursor = conn.execute("select channel_id, start_date from schedule;")
+    rows = cursor.fetchall()
+    cur_schedules = {}
+    for x in rows:
+      chan_id = x[0]
+      start_date = x[1]
+      if chan_id not in cur_schedules:
+        cur_schedules[chan_id] = []
+      if start_date not in cur_schedules[chan_id]:
+        cur_schedules[chan_id].append(start_date)
+    print(f"current schedules by chan id are {cur_schedules}")
+    return cur_schedules
+
+def get_program_sd_programids(conn):
+    cursor = conn.execute("select sd_programid from program;")
+    rows = cursor.fetchall()
+    program_ids = []
+    for x in rows:
+      program_ids.append(x[0])
+#    print(f"current program sd ids are {program_ids}")
+    return program_ids
 
 def add_schedules(schedules):
     conn = sqlite3.connect(DB_PATH)
-#INSERT INTO schedule (channel_id, start_date, end_date) VALUES (1, '2026-01-12T06:00:00+0000', '2026-01-12T07:00:00+0000');
+    cur_channels = get_channels(conn)
+    schedule_dates_by_chan_id = get_schedule_dates_by_chan_id(conn)
+
+    cursor = conn.cursor()
+    for schedule in schedules:
+      sched_station_id = schedule.get('stationID')
+      print(f'new schedule record for station id: {sched_station_id}')
+      if sched_station_id not in cur_channels.values():
+        print(f'skipping schedule {sched_station_id} - it is for a channel we dont know about')
+        continue
+      chan_id = [key for key,val in cur_channels.items() if val == sched_station_id][0] # doing this in memory to avoid a flood of sql calls
+
+      for program in schedule.get('programs', []):
+        sched_start_date = program.get('airDateTime')
+        if chan_id in schedule_dates_by_chan_id and sched_start_date in schedule_dates_by_chan_id[chan_id]:
+          print(f'  skipping preexisting schedule for {sched_station_id}  {sched_start_date}')
+          continue  # already scheduled
+        # TODO build the sched end date and test on date in range, also add end date to table below
+#        print(f'adding to schedule ===== {sched_station_id}  {sched_start_date}')
+        cursor.execute(f"INSERT INTO schedule (channel_id, start_date, end_date) VALUES (?, ?, ?);", (chan_id, sched_start_date, sched_start_date))
+
     conn.commit(); 
     conn.close()
     print('schedules add complete')
 
 def add_programs(programs):
+    add_counter = 0
     conn = sqlite3.connect(DB_PATH)
-#INSERT INTO program (schedule_id, sd_programid, title, description, genres, original_air_date, season, episode)
-#           VALUES (1, 'EP017254570124', 'Father Brown', 'Father Brown...', 'series,Drama,Crime', '2025-01-10', 11, 2);
+    cursor = conn.cursor()
+    channels = get_channels(conn)
+    existing_sd_program_ids = get_program_sd_programids(conn)
+    for program in programs:
+      pgm_ins_str = "INSERT INTO program (sd_programid, title, description, genres, original_air_date, season, episode) values (?,?,?,?,?,?,?);"
+
+      sd_program_id = program.get('programID')
+      if sd_program_id in existing_sd_program_ids:
+        continue
+      existing_sd_program_ids.append(sd_program_id)
+      title = ''
+      if program.get('titles'):
+        first = program.get('titles')[0]
+        title = first.get('title120')
+      description = ''
+      if program.get('descriptions'):
+        if program.get('descriptions').get('description1000'):
+          first = program.get('descriptions').get('description1000')[0]
+          description = first.get('description')
+        elif program.get('descriptions').get('description100'):
+          first = program.get('descriptions').get('description100')[0]
+          description = first.get('description')
+      genres = ''
+      if program.get('genres'):
+        genres = ','.join(program.get('genres'))
+      oad = program.get('originalAirDate')
+      season = ''
+      episode = ''
+      for md in program.get('metadata', []):
+        if md.get('TVmaze'):
+          season = md.get('TVmaze').get('season')
+          episode = md.get('TVmaze').get('episode')
+        elif md.get('Gracenote'):
+          season = md.get('Gracenote').get('season')
+          episode = md.get('Gracenote').get('episode')
+#      print(f'adding program ===== {sd_program_id} - {title}')
+      if not title:
+        continue # assume empty record
+      cursor.execute(pgm_ins_str, (sd_program_id, title, description, genres, oad, season, episode))
+      add_counter += 1
     conn.commit(); 
     conn.close()
-    print('programs add complete')
+    print(f'{len(programs)} programs processed, {add_counter} added')
 
 def add_persons(persons):
     conn = sqlite3.connect(DB_PATH)
@@ -133,13 +247,11 @@ def get_token():
       raise Exception('cannot get token from schedule direct')
     return token 
 
-
 def gather_schedule():
     token = get_token()
 
     status_url = f"{SD_URL}/status"
     status_headers = {'token': token}
-    print(f'status headers is {status_headers}')
     resp = requests.get(status_url, headers=status_headers)
     if not resp.ok:
       raise Exception('cannot get status')
@@ -147,7 +259,6 @@ def gather_schedule():
     if not lineups:
       raise Exception('no lineups found for sd account')
     lineup_id = lineups[0].get('lineup')
-    print(f'lineup id is {lineup_id}')
     
     channels_url = f"{SD_URL}/lineups/{lineup_id}"
     resp = requests.get(channels_url, headers=status_headers)
@@ -200,7 +311,7 @@ def gather_schedule():
 # NOW PERSIST THEM
     add_channels(channels)
 #    add_schedules(schedules)
-#    add_programs(programs)
+    add_programs(programs)
 
 #    add_persons(persons)
 
